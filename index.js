bc4f0029c4c0d30c3ed3658d507fce53f1978c4f
Aulas Ingeniería <aulasingenieria@javeriana.edu.co>
2023-10-21 11:02:52 -0500

    API initial config

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 
 app.listen(3000, () => {
-    console.log("El puerto se inicializa en el puerto 3000");
+    console.log("El servidor se inicializa en http://localhost:3000");
 
 })
